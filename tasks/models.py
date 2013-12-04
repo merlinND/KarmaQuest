1bbeccd10c04c2098ea84f9f0e45f3d8094b3bca
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-04 21:13:40 +0100

    Add task models base

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User as BaseUser
 
-# Create your models here.
+class User(BaseUser):
+    level = models.PositiveIntegerField(default=0)
+    points = models.PositiveIntegerField(default=0)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+
+class Task(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    reward = models.PositiveIntegerField()
+    location = models.CharField(max_length=200)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    tags = models.ManyToManyField(Tag)
+    # TODO repeatability ?
+
+class Picture(models.Model):
+    task = models.ForeignKey(Task)
+    image = models.ImageField(upload_to='tasks/pictures')

41536393df4644bb1ada51fd7afbbd098fa48767
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-01-14 11:55:39 +0100

    Templates : various enhancements

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
         url(r'^$', RedirectView.as_view(url='/tasks/', permanent=False)),
         url(r'^tasks/pictures/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': 'tasks/pictures/' }),
+        url(r'^user/pictures/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': 'user/pictures/' }),
         url(r'^tasks/', include('tasks.urls', namespace='tasks')),
         url(r'^user/', include('user.urls', namespace='user')),
         url(r'^admin/', include(admin.site.urls)),
